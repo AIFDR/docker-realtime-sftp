781ffc2cd6a365a157a3dc29a59c57fdb711b3f6
Tim Sutton <tim@linfiniti.com>
2014-07-17 17:03:13 +0200

    Another small fix for run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 SHAKEDIR=/home/realtime/shakemaps
+
+docker.io kill inasafe-realtime-sftp
+docker.io rm inasafe-realtime-sftp
+
 mkdir -p $SHAKEDIR
 docker.io run --name='inasafe-realtime-sftp' \
 	-v $SHAKEDIR:/shakemaps \
@@ -8,7 +12,7 @@ docker.io run --name='inasafe-realtime-sftp' \
 	-d -t AIFDR/sftp-realtime
 
 echo "You can copy files into this container with these credentials:"
-docker cp inasafe-realtime-sftp:/credentials .
+docker.io cp inasafe-realtime-sftp:/credentials .
 cat credentials
 rm credentials
 
