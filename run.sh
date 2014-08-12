bf89de9d41e90dae7a239080feee0a94630531fa
Akbar Gumbira <akbargumbira@gmail.com>
2014-08-12 11:33:44 +0700

    Update run script.

--- run.sh
+++ run.sh
@@ -3,18 +3,16 @@
 SHAKEDIR=/home/realtime/shakemaps
 SFTP_IMAGE=docker-realtime-sftp
 
-docker kill inasafe-realtime-sftp
-docker rm inasafe-realtime-sftp
+docker kill ${SFTP_IMAGE}
+docker rm ${SFTP_IMAGE}
 
 mkdir -p $SHAKEDIR
-docker run --name='inasafe-realtime-sftp' \
+docker run --name="${SFTP_IMAGE}" \
 	-v $SHAKEDIR:$SHAKEDIR \
 	-p 9222:22 \
 	-d -t aifdr/${SFTP_IMAGE}
 
 echo "You can copy files into this container with these credentials:"
-docker cp inasafe-realtime-sftp:/credentials .
+docker cp ${SFTP_IMAGE}:/credentials .
 cat credentials
 rm credentials
-
-
